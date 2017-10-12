429fa18635feda115083607fabbb8dee913b1cea
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2017-10-12 14:28:50 -0700

    Shrinking circles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 
-// when a key is pressed, draw a circle 
+// array to store all circles 
+var circles = [];
+
+// when a key is pressed, add circle to circle array, draw circle 
 function onKeyDown(event) {
     var maxPoint = new Point(view.size.width, view.size.height);
     var randomPoint = Point.random();
     var point = maxPoint * randomPoint;
-    new Path.Circle(point, 50).fillColor = "purple";
+
+    var newCircle = new Path.Circle(point, 500);
+    newCircle.fillColor = "purple";
+
+    circles.push(newCircle);
+}
+
+function onFrame(event) {
+    // each frame, change fill color of each circle by 1
+    circles.forEach(function(circle) {
+        circle.fillColor.hue += 1; 
+        circle.scale(.96);
+    });
 }
